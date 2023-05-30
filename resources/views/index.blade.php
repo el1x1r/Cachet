700340d88c85e366dbc7331705d2a035de4f1ccf
Jorge Lipa <jorge.lipa@jalasoft.com>
2023-05-29 22:06:52 -0400

    Move incident list position to top after status messages

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 @include('partials.modules.messages')
 @include('partials.modules.status')
+@include('partials.modules.timeline')
 @include('partials.about-app')
 @include('partials.modules.components')
 @include('partials.modules.metrics')
 @include('partials.modules.stickied')
 @include('partials.modules.scheduled')
-@include('partials.modules.timeline')
 @stop
 
 @section('bottom-content')
